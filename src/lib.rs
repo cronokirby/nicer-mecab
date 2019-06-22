6d371b55d4b008768dc22f71b37727d3bbdd20d4
Lúcás Meier <cronokirby@gmail.com>
2019-06-22 21:29:03 +0200

    Add function to get raw mecab output

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+use mecab::Tagger;
+
+pub fn parse_sentence(sentence: &str) -> String {
+    let mut tagger = Tagger::new("");
+    tagger.parse_nbest_init(sentence);
+    tagger.next().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parse_sentence_works_for_single_words() {
+        let sentence = "猫";
+        let result = "猫\t名詞,一般,*,*,*,*,猫,ネコ,ネコ\nEOS\n";
+        assert_eq!(result, parse_sentence(sentence));
     }
 }
