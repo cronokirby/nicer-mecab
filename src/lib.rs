6da54b6bd81675146fccab36c2f000e9efb8ef1b
Lúcás Meier <cronokirby@gmail.com>
2019-06-22 22:47:30 +0200

    Split off the raw part of a sentence from mecab's result

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
 use mecab::Tagger;
 
-pub fn parse_sentence(sentence: &str) -> String {
+/// A morpheme represents a single part of a sentence with meaning.
+///
+/// Sentences are split into individual morphemes, which form the sentence together.
+/// Each morpheme contains the raw part split off from the sentence as well as
+/// information about the morpheme.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Morpheme {
+    /// The raw text associated with this morpheme.
+    raw: String,
+}
+
+/// This represents the information parsed by the analyzer.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Sentence {
+    /// The list of morphemes composing the sentence.
+    morphemes: Vec<Morpheme>,
+}
+
+pub fn parse_sentence(sentence: &str) -> Sentence {
     let mut tagger = Tagger::new("");
     tagger.parse_nbest_init(sentence);
-    tagger.next().unwrap()
+    let mecab_out = tagger.next().unwrap();
+    let mut morphemes = Vec::new();
+    for l in mecab_out.lines() {
+        if l == "EOS" {
+            break;
+        }
+        // We know that this index exists, based on mecab output
+        let tab_index = l.find('\t').unwrap();
+        let (raw, _rest) = l.split_at(tab_index);
+        morphemes.push(Morpheme {
+            raw: raw.to_string(),
+        });
+    }
+    Sentence { morphemes }
 }
 
 #[cfg(test)]
@@ -13,7 +44,10 @@ mod tests {
     #[test]
     fn parse_sentence_works_for_single_words() {
         let sentence = "猫";
-        let result = "猫\t名詞,一般,*,*,*,*,猫,ネコ,ネコ\nEOS\n";
+        let raw = String::from("猫");
+        let result = Sentence {
+            morphemes: vec![Morpheme { raw }],
+        };
         assert_eq!(result, parse_sentence(sentence));
     }
 }
